8b6c81dbbb27516814982e734dad7a7ad20173eb
GowthamJ-05 <141749637+GowthamJ-05@users.noreply.github.com>
2024-06-26 16:00:31 +0530

    Update submitTask_alias.sh

--- submitTask_alias.sh
+++ submitTask_alias.sh
@@ -2,37 +2,31 @@
 
 mentor_func ()
 {
-    for grp in $grouplist
-    do
-        if [[ $grp == "web_mentors_grp" ]]
-        then
-            domain=web
-            break
-        fi
-        if [[ $grp == "app_mentors_grp" ]]
-        then
-            domain=app
-            break
-        fi
-        if [[ $grp == "sysad_mentors_grp" ]]
-        then
-            domain=sysad
-            break
-        fi
-    done
+    if echo $(groups) | grep -q '\bweb_mentors_grp\b'
+    then
+        domain=web
+    elif echo $(groups) | grep -q '\bapp_mentors_grp\b'
+    then
+        domain=app
+    elif echo $(groups) | grep -q '\bsysad_mentors_grp\b'
+    then
+        domain=sysad
+    fi
     
-    mentee_list=$(awk 'BEGIN{ ORS=" " } $0 !~/Rollno Name/{ print $2;} END{}' "~/AlottedMentees.txt") 
+    mentee_list=$(awk 'BEGIN{ OFS="_";ORS=" "} $0 !~/Rollno Name/{ print $2,$1 ;}' "$HOME/Alottedmentees.txt") 
     for mentee in $mentee_list
     do
-        dir_location="~/../../../mentees/$mentee/$domain"
+        dir_location="$HOME/../../../mentees/$mentee/$domain"
         declare -a task_completed_arr
         for i in {1..3}
         do
             task_location="$dir_location/Task$i"
             if [[ -d "$task_location" ]]
             then
-                cd ~/submitted_tasks/task$i  
-                ln -s ~/../../../mentees/$mentee/$domain/Task$i $mentee
+                if [[ ! -h "$HOME/submitted_tasks/task$i/$mentee" ]]
+                then
+                    ln -s $HOME/../../../mentees/$mentee/$domain/Task$i $HOME/submitted_tasks/task$i/$mentee
+                fi
                 if [[ -z "$task_location/*" ]]
                 then
                     task_completed_arr[$i]='n'
@@ -43,37 +37,25 @@ mentor_func ()
                 task_completed_arr[$i]='n'
             fi
         done
-        declare -i column
-        column=$(awk -v domain="$domain" 'BEGIN{column=1; FS=","; OFS=","} $0 ~/^Tasks_domain/{for (i = 1; i <= NF; i++) {if ($i != domain){column++} else {print column ;break}}}' $dir_location/../task_completed.txt)
-        modified_data=$(awk -v col=$column -v val1=${task_completed_arr[1]} -v val2=${task_completed_arr[2]} -v val3=${task_completed_arr[3]}'BEGIN{task[2]=val1;task[3]=val2;task[4]=val3; FS=","; OFS=","} $0 !~/^Tasks_domain/{$col=task[NR];} {print $1,$2,$3,$4 } END{}' $dir_location/../task_completed.txt)
+        modified_data=$(awk -v col="$column" -v val1="${task_completed_arr[1]}" -v val2="${task_completed_arr[2]}" -v val3="${task_completed_arr[3]}" 'BEGIN {task[2] = val1;task[3] = val2;task[4] = val3;FS = ",";OFS = ",";ORS="\n"}$0 !~ /^Task_Domain/ {$col = task[NR];}{print $1,$2,$3,$4;}' "$dir_location/../task_completed.txt")
         echo -e "$modified_data" > $dir_location/../task_completed.txt
     done
 }
 
-
 mentee_func ()
 {
-    mentee_preference=$(awk 'BEGIN{FS="->"} {if (NR == 2) {for (i=1;i<=NF:i++) print $i}}'"/home/core/mentees/$(whoami)/domain_pref.txt")
-    for i in $mentee_preference
-    do
-        if [[ $1 == $i]]
-        then
-            condition=0
-        fi 
-    done
-    if [[ $condition -eq 0 ]]
+    mentee_preference=$(awk 'BEGIN{FS="->"} {if (NR == 2) {for (i=1;i<=NF;i++) print $i}}' "/home/core/mentees/$(whoami)/domain_pref.txt")
+    condition=1
+    if echo $1 | grep -q "${mentee_preference}"
     then
         echo "$1:" > /home/core/mentees/$(whoami)/task_submitted.txt
-        declare -a taskarr
         for i in {1..3}
         do
-            read -p "Have you submitted the task$i (Y/n) " fetch
-            taskarr[$i]=$fetch
-            echo -e "\tTask$i: ${taskarr[$i]}" >> /home/core/mentees/$(whoami)/task_submitted.txt
-            if [[ ${taskarr[$i]} == 'y' ]] || [[ ${taskarr[$i]} == 'Y' ]]
+            read -p "Have you submitted the task$i (Y/n) " reply
+            echo -e "\tTask$i: $reply" >> /home/core/mentees/$(whoami)/task_submitted.txt
+            if  [[ $reply == 'y' || $reply == 'Y' ]] && ![[ -d "/home/core/mentees/$(whoami)/$1/Task$i" ]]
             then
-                cd /home/core/mentees/$(whoami)/$1
-                mkdir Task$i
+                mkdir /home/core/mentees/$(whoami)/$1/Task$i 
             fi
         done
         echo "Task folders have been created"
@@ -84,6 +66,11 @@ mentee_func ()
 
 check_mentee_func ()
 {
+    if [[ $# -eq 0 ]]
+    then
+        echo "USAGE: submitTask domain[web|app|sysad]..."
+        return
+    fi
     for i in $1 $2 $3
     do 
         match=1
@@ -100,43 +87,21 @@ check_mentee_func ()
 
         if [[ $match -eq 0 ]]
         then
-            echo mentee_func $j
+            mentee_func $j
         else 
             echo "Invalid parameters given. Provide valid Parameters only"
-            return 1
+            return
         fi
     done
-    return 0
+    return
 }
-
-
-
 
 submitTask_func ()
 {
-    inmentees=1
-    inmentor=1
-    grouplist=$(groups)
-    for grp in $grouplist
-    do
-        if [[ $grp == "mentees_grp" ]]
-        then
-            inmentees=0
-            break
-        fi
-    done
-    for grp in $grouplist
-    do
-        if [[ $grp == "mentors_grp" ]]
-        then
-            inmentor=0
-            break
-        fi
-    done
-    if [[ $inmentees -eq 0 && $inmentor -eq 1 ]]
+    if echo $(groups) | grep -q '\bmentees_grp\b'
     then
         check_mentee_func $1 $2 $3
-    elif [[ $inmentees -eq 1 && $inmentor -eq 0 ]]
+    elif echo $(groups) | grep -q '\bmentors_grp\b'
     then 
         mentor_func
     else
